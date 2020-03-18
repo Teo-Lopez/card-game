e8a886f981a14bf446b992d1ec12c7746bee3df0
Teo <teodorolg130@gmail.com>
2020-03-18 18:34:55 +0100

    architecture - changed folder structure and added backend

---
+++ client/src/components/Board.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import Slot from "./Slot";
+import styled from "styled-components";
+import { DndProvider } from "react-dnd";
+import Backend from "react-dnd-html5-backend";
+import BoardSection from "./BoardSection";
+import { Row, Col } from "react-bootstrap";
+import { deck01 } from "../utils/cards/Cards";
+import background from "../assets/wood-bg.jpg";
+import Deck from "./Deck";
+
+const BoardWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: whitesmoke;
+  background-image: url(${background});
+  background-size: cover;
+  display: flex;
+`;
+
+function Board() {
+  const [ownPositions, setOwnPositions] = useState(Array(8).fill(null));
+  const [drawedCard, setDrawedCard] = useState(null);
+
+  const drawCard = () => {
+    const randNum = Math.floor(Math.random() * deck01.length);
+    const drawedCard = deck01[randNum];
+    removeCard(randNum, deck01);
+    setDrawedCard(drawedCard);
+  };
+
+  useEffect(() => {
+    const playCard = (drawedCard, idx = ownPositions.indexOf(null)) => {
+      if (idx === -1 || drawedCard == null) return;
+      const ownPositionsCopy = [...ownPositions];
+      ownPositionsCopy.splice(idx, 1, drawedCard);
+      setDrawedCard(null);
+      setOwnPositions(ownPositionsCopy);
+    };
+    playCard(drawedCard);
+  }, [drawedCard, ownPositions]);
+
+  const removeCard = (idx, arr) => {
+    arr.splice(idx, 1);
+    return arr;
+  };
+
+  const attack = (idx, newPos) => {
+    if (idx === newPos) return;
+    // else if()
+    const ownPositionsCopy = [...ownPositions];
+    ownPositionsCopy[idx].attack(ownPositionsCopy[newPos]);
+    const newownPositions = ownPositionsCopy.map(elm => (elm == null || elm.life <= 0 ? null : elm));
+    setOwnPositions(newownPositions);
+  };
+
+  const move = (idx, newPos) => {
+    if (idx === newPos) return;
+    const ownPositionsCopy = [...ownPositions];
+    ownPositionsCopy.splice(newPos, 1, ownPositionsCopy[idx]);
+    ownPositionsCopy.splice(idx, 1, null);
+
+    setOwnPositions(ownPositionsCopy);
+  };
+
+  return (
+    <DndProvider backend={Backend}>
+      <BoardWrapper>
+        <Row style={{ width: "100%" }} noGutters={false} className="justify-content-around">
+          {ownPositions.map((elm, idx) => (
+            <Col md={3}>
+              <BoardSection move={move} attack={attack} char={elm} idx={idx} key={idx}>
+                {elm != null ? <Card char={elm} idx={idx}></Card> : <Slot empty idx={idx}></Slot>}
+              </BoardSection>
+            </Col>
+          ))}
+        </Row>
+        <Deck drawCard={drawCard} />
+      </BoardWrapper>
+    </DndProvider>
+  );
+}
+
+export default Board;
---
+++ client/src/components/Card.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+import { useDrag } from "react-dnd";
+import { ItemTypes } from "../utils/dragItems";
+
+const Card = props => {
+  const { power, life, maxLife, name, img } = props.char;
+
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: ItemTypes.CARD, idx: props.idx, char: props.char },
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging()
+    })
+  });
+
+  const CardWrapper = styled.div`
+    margin: 0 auto;
+    width: 90%;
+    padding: 0 5%;
+    height: 250px;
+    background-color: rgba(240, 240, 240, 0.5);
+    /* border: 1px solid grey; */
+    box-sizing: content-box;
+    h3 {
+      font-size: 16px;
+    }
+  `;
+
+  const CardTitle = styled.div`
+    text-align: center;
+    height: 5%;
+    margin-bottom: 15px;
+  `;
+
+  const CardStats = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 10%;
+  `;
+
+  const CardImage = styled.div`
+    width: 100%;
+    height: 80%;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  `;
+
+  return (
+    <CardWrapper
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: isDragging ? 25 : "initial",
+        fontWeight: "bold",
+        cursor: "move"
+      }}
+    >
+      <CardTitle>
+        <h3>
+          <strong>{name}</strong>
+        </h3>
+      </CardTitle>
+      <CardStats>
+        <div>
+          <p>Pow: {power}</p>
+        </div>
+        maxLife
+        <div>
+          <p>HP: {life}</p>
+        </div>
+      </CardStats>
+      <CardImage>
+        <img alt={name} src={img} />
+      </CardImage>
+    </CardWrapper>
+  );
+};
+
+export default Card;

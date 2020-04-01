09f6cfa3167067bff837c5bc277e93cc0db99d18
Teo <teodorolg130@gmail.com>
2020-04-01 21:04:49 +0200

    deploy

---
+++ server/public/precache-manifest.8fd9b7e627608645af8f0baa7bb59c79.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a576f20c59aa7359e772b097ef9b3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c48a96e4a977f245284",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6d90314726b297bbbc25",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c48a96e4a977f245284",
+    "url": "/static/js/2.9de97f1a.chunk.js"
+  },
+  {
+    "revision": "647c25ca9e3ea0b71bfc55fa250b5834",
+    "url": "/static/js/2.9de97f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d90314726b297bbbc25",
+    "url": "/static/js/main.a1662fdd.chunk.js"
+  },
+  {
+    "revision": "df67eaa4b9fbf7b5cd49",
+    "url": "/static/js/runtime-main.a629fecb.js"
+  },
+  {
+    "revision": "7bc064b684cdc152575c0bebca9b9802",
+    "url": "/static/media/clash.7bc064b6.wav"
+  },
+  {
+    "revision": "509edcbe650928811b9c0f05aeed6434",
+    "url": "/static/media/cover.509edcbe.png"
+  },
+  {
+    "revision": "a98cce736e3ea63ea220c7724d9d9d33",
+    "url": "/static/media/leena.a98cce73.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8331c8eb1d88c0c9d21d2cd785ffe60f",
+    "url": "/static/media/midoriya.8331c8eb.png"
+  },
+  {
+    "revision": "23f4643b2a45058608b9f3b6e805fffe",
+    "url": "/static/media/wood-bg.23f4643b.jpg"
+  }
+]);

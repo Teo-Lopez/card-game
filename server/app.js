ddbeb42a420f6b57a5ee1cd793e1226452d1649e
Teo <teodorolg130@gmail.com>
2020-04-01 20:44:10 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -13,13 +13,12 @@ require("./configs/locals.config")(app);
 require("./configs/session.config")(app);
 app.use(flash());
 
+app.use((req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 const index = require("./routes/index");
 app.use("/", index);
 
 const authRoutes = require("./routes/auth");
 app.use("/auth", authRoutes);
-
-app.use((req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
 module.exports = app;
